6bfc48267d187f59a7e218f9d06c45b19331730e
Ushee <michaela.ushee@gmail.com>
2022-02-22 18:32:10 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -3,4 +3,7 @@ let věk = 30;
 let váha = 67;
 let výška = 171;
 let bmi = váha / výška
+
+let jmeno = prompt("Jak se jmenujes?")
+
 console.log("jmenuji se" + jmeno + "je mi" + věk + "vážím" + váha + "měřím" + výška + "moje BMI je" + bmi)
