80ae05ff2965b0f9d2c4429a44be6eef033b45a8
Ushee <michaela.ushee@gmail.com>
2022-02-22 18:19:23 +0100

    1 file modified

--- ukol1.js
+++ ukol1.js
@@ -1,2 +1,6 @@
-console.log('Ahoj');
-
+let jmeno = 34;
+let věk 30;
+let váha = 67;
+let výška = 171;
+let bmi = vaha / výška
+console.log("jmenuji se" + jméno + "je mi" + věk + "vážím" + váha + "měřím" + výška + "moje BMI je" + bmi)
